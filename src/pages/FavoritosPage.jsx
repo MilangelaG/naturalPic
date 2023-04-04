b03519bcdb9555cfd215efc2569d471db6a63b85
Milangela Grimaldo <titagrimaldoo@gmail.com>
2023-04-03 22:32:00 -0400

    Funcionalidad Favoritos

--- src/pages/FavoritosPage.jsx
+++ src/pages/FavoritosPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MyContext } from '../context/MyContext'
+import Heart from '../components/Heart.jsx'
 
 const FavoritosPage = () => {
+
+  const  {fotos, setFotos} = useContext(MyContext);
+
+  const handleClick = (id) => {
+    const fotoIndex = fotos.findIndex((f) => f.id === id);
+    fotos[fotoIndex].favorito = !fotos[fotoIndex].favorito;
+    setFotos([...fotos]);
+  };
+
+  const filterFavorites = (coleccion) => {
+    return coleccion.filter(item => item.favorito);
+  };
+
   return (
-    <div>
-        <h2>Pagina de Favoritos</h2>
+    <div className='galeria grid-columns-5 p-3'>
+       {filterFavorites(fotos).map(foto => (
+         <div
+         onClick={() => handleClick(foto.id)}
+         className='foto' key={foto.id}
+         style={{ backgroundImage: `url(${foto.src})`}} >
+         <Heart filled={foto.favorito} />
+        </div>
+       ))}
     </div>
   )
 }
